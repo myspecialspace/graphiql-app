3ae0090cf01b6b45e9bdccc7ac3e876e4b09d311
myspecialspace <myspecialspace@users.noreply.github.com>
2023-04-25 13:43:59 +0300

    feat: setup i18n for localization, react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css'
+import { useTranslation } from 'react-i18next';
+import './App.css';
+import './i18n/config';
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
-  )
+  const { t } = useTranslation();
+  return <h1 className="text-3xl font-bold underline">{t('Привет мир')}</h1>;
 }
 
-export default App
+export default App;
---
+++ src/i18n/config.ts
@@ -0,0 +1,19 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import * as en from './en.json';
+import * as ru from './ru.json';
+
+export const resources = {
+  en: {
+    translation: en,
+  },
+  ru: {
+    translation: ru,
+  },
+};
+
+i18next.use(initReactI18next).init({
+  lng: 'en',
+  debug: true,
+  resources,
+});
