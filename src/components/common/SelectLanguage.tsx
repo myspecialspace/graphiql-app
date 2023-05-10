d87199399a2570c8b5f7887f107f344652460dda
ElizabethT7 <tymofieievai@gmail.com>
2023-05-10 14:18:55 +0300

    feat: add select language button

---
+++ src/components/common/SelectLanguage.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+
+export const SelectLanguage: FC = () => {
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (lng: string) => {
+    localStorage.setItem('lang', lng);
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <select
+      name="language"
+      id="language"
+      defaultValue={localStorage.getItem('lang') || 'English'}
+      onChange={(e) => changeLanguage(e.target.value)}
+      className="h-10 w-36 rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 focus:outline-none sm:text-sm"
+    >
+      <option className="h-10 w-full" value="en">
+        {t('en')}
+      </option>
+      <option className="h-10 w-full" value="ru">
+        {t('ru')}
+      </option>
+    </select>
+  );
+};
