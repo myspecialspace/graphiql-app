0a18d42e28406ccb3689b4c4b0122734eea82a2c
myspecialspace <myspecialspace@users.noreply.github.com>
2023-05-11 17:27:01 +0300

    feat: add user types, slices/user, change store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { mainSlice } from './slices/main';
+import { userSlice } from './slices/user';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 export const store = configureStore({
   reducer: {
     main: mainSlice.reducer,
+    user: userSlice.reducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
