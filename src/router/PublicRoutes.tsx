f00571e7a63f31ab5bc05852d7440ea8e6ab1c4a
myspecialspace <myspecialspace@users.noreply.github.com>
2023-05-12 15:21:15 +0300

    refactor: change public routes logic

--- src/router/PublicRoutes.tsx
+++ src/router/PublicRoutes.tsx
@@ -1,14 +1,18 @@
-import { SIGNIN_ROUTE, SIGNUP_ROUTE } from '@/helpers/constants';
+import { MAIN_ROUTE, SIGNIN_ROUTE, SIGNUP_ROUTE } from '@/helpers/constants';
 import { NotFoundPage } from '@/pages/NotFound/NotFound';
 import { SigninPage } from '@/pages/Signin/Signin';
 import { SignupPage } from '@/pages/Signup/Signup';
 import { WelcomePage } from '@/pages/Welcome/Welcome';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
-const PrivateRoutes = () => {
+const PublicRoutes = () => {
   return (
     <Routes>
       <Route path="" element={<WelcomePage />} />
+      <Route
+        path={MAIN_ROUTE}
+        element={<Navigate to={SIGNIN_ROUTE} replace />}
+      />
       <Route path={SIGNUP_ROUTE} element={<SignupPage />} />
       <Route path={SIGNIN_ROUTE} element={<SigninPage />} />
       <Route path="*" element={<NotFoundPage />} />
@@ -16,4 +20,4 @@ const PrivateRoutes = () => {
   );
 };
 
-export default PrivateRoutes;
+export default PublicRoutes;
