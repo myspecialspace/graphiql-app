e83ccadbdaf39208872a28f1c998361134920fd7
Clearenough <92729374+Clearenough@users.noreply.github.com>
2023-05-14 13:18:50 +0300

    fix: add Layout import

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,5 +1,6 @@
 import { QueryEditor } from '@/components/QueryEditor/QueryEditor';
 import QueryResponse from '@/components/QueryResponse/QueryResponse';
+import { Layout } from '@/components/common/Layout';
 import { FC, useState } from 'react';
 import { SideBar } from '@/components/SideBar/SideBar';
 
