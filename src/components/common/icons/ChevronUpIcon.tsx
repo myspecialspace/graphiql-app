5d8d8d725209bc29f6c20ed95ff5cbfceaaf9bb0
ElizabethT7 <tymofieievai@gmail.com>
2023-05-15 18:45:02 +0300

    feat: add variables/headers menu

---
+++ src/components/common/icons/ChevronUpIcon.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+
+export const ChevronUpIcon: FC = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="#374151"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 15.75l7.5-7.5 7.5 7.5"
+      />
+    </svg>
+  );
+};
