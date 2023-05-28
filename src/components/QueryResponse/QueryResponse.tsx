54b90e45b38030e364565c0f59770eaacdb0e610
ElizabethT7 <tymofieievai@gmail.com>
2023-05-28 17:41:48 +0300

    fix: correct style

--- src/components/QueryResponse/QueryResponse.tsx
+++ src/components/QueryResponse/QueryResponse.tsx
@@ -9,12 +9,12 @@ const QueryResponse = ({ response }: QueryResponseProps) => {
   return (
     <ReactCodeMirror
       value={response}
-      height="90%"
+      height="100%"
       theme="light"
       placeholder={'Response here'}
       basicSetup={true}
       extensions={[json()]}
-      className={'grow text-left max-w-[50%]'}
+      className={'grow text-left w-full sm:max-w-[50%] max-h-[81vh]'}
       readOnly
     />
   );
